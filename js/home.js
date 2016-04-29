d4fc960bc955e3b3cfbcd936e1f0eec28cc9adbe
rensramos <rensramos17@gmail.com>
2016-04-29 19:16:19 +0800

    update key

--- js/home.js
+++ js/home.js
@@ -7,55 +7,7 @@ main.init = function(){
     main.initCountDown();
     main.onMenuClick();
     main.onClickBrandLogo();
-    main.initGoogleMap();
 }
-
-main.initGoogleMap = function(){
-    var mapLocation = {lat: 14.370429, lng: 121.008782};
-    var customMapType = new google.maps.StyledMapType([
-        {
-            featureType: 'road',
-            stylers: [
-                {visibility: 'simplified'},
-                {gamma: 0.5},
-                {weight: 0.5},
-                { hue: "#333333" }
-            ]
-        },
-        {
-            elementType: 'labels',
-            stylers: [{visibility: 'on'}]
-        },
-        {
-            featureType: 'water',
-            stylers: [{color: '#2d2d2d'}]
-        }
-    ], {
-        name: 'Custom Style'
-    });
-    var customMapTypeId = 'custom_style';
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: mapLocation,
-        mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
-        }
-    });
-
-    var image = '/img/map-icon/century.png'
-
-    var marker = new google.maps.Marker({
-        position: mapLocation,
-        map: map,
-        center: mapLocation,
-        icon: image
-    });
-
-    map.mapTypes.set(customMapTypeId, customMapType);
-    map.setMapTypeId(customMapTypeId);
-
-};
 
 main.onClickBrandLogo = function(){
     var brandlogo = $('nav.navbar-default .container .navbar-header .navbar-brand');
