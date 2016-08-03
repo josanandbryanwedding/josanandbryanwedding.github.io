f29c1e8660d3dfed7a5f5a5dba3b91be3cc4d1fb
rensramos <rensramos17@gmail.com>
2016-08-03 13:56:38 +0800

    Add instagram section

--- js/home.js
+++ js/home.js
@@ -9,7 +9,35 @@ main.init = function(){
     main.onClickBrandLogo();
     main.onClickIntroButton();
     main.onFormSubmit();
+    main.instagramFeed();
 
+};
+
+main.instagramFeed = function () {
+    $.getScript('/js/instafeed.min.js', function () {
+        if (Instafeed) {
+            new Instafeed({
+                get: 'tagged',
+                tagName: 'MYSTERYMANILA',
+                clientId: '0cd51b94022a49f0be1e4b114f24a9ea',
+                accessToken: '1162756535.0cd51b9.7b8b57dd36494ff08f9e4e5c976742b7',
+                limit: 12,
+                sortBy: 'most-recent',
+                template: '<a href="{{link}}" target="_blank" class="col-md-2 col-sm-2 col-xs-6"><img src="{{image}}" /></a>',
+                resolution: 'low_resolution',
+                success: function(feed){
+                    var data = feed.data.reverse();
+                    $('.placeholder').each(function(index, placeholder){
+                        var model = data.pop();
+                        $(placeholder).html(
+                            '<a href="'+ model.link +'" target="_blank">'
+                            + '<img src="'+ model.images.low_resolution.url +'" />'
+                            + '</a>').hide().fadeIn();
+                    });
+                }
+            }).run();
+        }
+    });
 };
 
 main.onFormSubmit = function (){
