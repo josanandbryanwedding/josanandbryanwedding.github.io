58c5596ae6bc344d4e4aa1ae7212fe37c0af3c47
rensramos <rensramos17@gmail.com>
2016-08-17 13:17:37 +0800

    Update script and data

--- _script/file.groovy
+++ _script/file.groovy
@@ -24,13 +24,16 @@ String baseUrl = url.take(url.indexOf('/', url.indexOf('://')+3))
 
 new URL(url).withReader (ENCODING) { reader ->
     def document = parser.parse(reader)
-
+    int counter = 0
     document.'**'.findAll { it.@class == 'flip-entry' }.each{
+      counter = counter+1
       Map file = [:]
       String id = it.@id.toString().replace('entry-','')
+      file.slugId = counter
       file.permalinkImage = "http://drive.google.com/uc?export=view&id=${id}"
       file.url = "https://drive.google.com/file/d/${id}/view?pli=1"
       files << file
+
     }
 }
 
@@ -39,8 +42,9 @@ new URL(url).withReader (ENCODING) { reader ->
 File yaml = new File('files.yml')
 
 files.each{ file ->
-  yaml << "- slugId: \n"
+  yaml << "- slugId: ${file.slugId} \n"
   yaml << "  permaImage: ${file.permalinkImage}\n"
+  yaml << "  col: \n"
   yaml << "  url: ${file.url}\n\n"
 }
 
